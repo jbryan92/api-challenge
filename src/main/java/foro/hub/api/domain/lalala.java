28cc5cf34591b04d78af1d8f576f67100473f073
jbryan <jbryangf19@gmail.com>
2025-08-10 19:01:31 -0500

    aviso de la clase lalala

--- src/main/java/foro/hub/api/domain/lalala.java
+++ src/main/java/foro/hub/api/domain/lalala.java
@@ -1,4 +1,7 @@
 package foro.hub.api.domain;
 
 public class lalala {
+    // Cree esto porque intellij es fastidioso y no me dejaba tener domain sin una class
+    // no funciona para nada, pero necesario para poder meter topico y usuario en una misma carpeta
+    // sin esta clase la carpeta domain aparecia apagada y no se por que
 }
